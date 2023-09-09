5f640a5fead0ea9007820b1121a97a307ef03bbe
shimaahriaz <shimaahriaz@gmail.com>
2023-09-09 16:47:48 -0700

    task1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 (Success)
+ * main - write to stdout
+ * Return: Always 0
  */
 int main(void)
 {
-	
-        fput("and that piece of art is useful\" - Dora korpar, 2023-8-9\n", stdout);
-        return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
