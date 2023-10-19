f47f23b70bc92984812af4989f0161f7e9f68666
shimaahriaz <shimaahriaz@gmail.com>
2023-10-19 09:03:11 -0700

    a

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * list_len - returns the number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+size_t nelem = 0;	
+for (; h != NULL; h = h->next)
+{
+nelem++;
+}
+return (nelem);
+}
