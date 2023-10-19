311b69c3df55d8b464f3dcad80afb41b258ef208
shimaahriaz <shimaahriaz@gmail.com>
2023-10-19 10:32:22 -0700

    a

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,13 +5,14 @@
  * @head: head of the linked list.
  * Return: no return.
  */
+
 void free_list(list_t *head)
 {
 list_t *current;
-do {
-current = head;
+while ((current = head) != NULL)
+{
 head = head->next;
 free(current->str);
 free(current);
-} while (head != NULL);
+}
 }
