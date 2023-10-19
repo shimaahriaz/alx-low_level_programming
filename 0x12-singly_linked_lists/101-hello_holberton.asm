aa9dce8d4c86f0376ad6705e5891eb63bfdc055f
shimaahriaz <shimaahriaz@gmail.com>
2023-10-19 10:44:48 -0700

    a

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,21 @@
-section .data
-    hello db "Hello, Holberton", 10
-    hello_len equ $ - hello
+extern printf
 
 section .text
-global _start
-_start:
-; Write the message to stdout (file descriptor 1)
-mov eax, 4         ; sys_write
-mov ebx, 1         ; file descriptor 1 (stdout)
-mov ecx, hello     ; pointer to the message
-mov edx, hello_len ; message length
-int 0x80           ; interrupt to invoke sys_write
-; Exit the program
-mov eax, 1         ; sys_exit
-mov ebx, 0         ; status: 0
-int 0x80           ; interrupt to invoke sys_exit
+global main
+
+main:
+push rbp
+
+mov rdi, fmt
+mov rsi, msg
+mov rax, 0
+call printf
+
+pop rbp
+
+mov rax, 0
+ret
+
+section .data
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
