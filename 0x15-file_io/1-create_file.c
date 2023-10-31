184423a03e56d331d179f44a445a50545cf3215b
shimaahriaz <shimaahriaz@gmail.com>
2023-10-31 04:31:44 -0700

    a

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+#include "main.h"
+/**
+ * create_file - Creates a file with specified permissions and content.
+ * @filename: The name of the file to create.
+ * @text_content: The content to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+int fd;
+int bytes_written;
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+if (text_content != NULL)
+{
+bytes_written = write(fd, text_content, strlen(text_content));
+if (bytes_written == -1)
+{
+close(fd);
+return (-1);
+}
+}
+close(fd);
+return (1);
+}
